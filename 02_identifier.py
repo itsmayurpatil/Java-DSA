2ce308f3a2793ac823dae8f04254d375e7c6c4a9
Mayur R. Patil <malpha1676@gmail.com>
2026-08-30 16:02:58 +0530

    Delete 02_identifier.py

--- 02_identifier.py
+++
@@ -1,9 +0,0 @@
-name = "MAYUR PATIL*"
-age = 25
-hobbies = ["reading", "coding", "gaming"]
-
-#here (name,age and hobbies) are variables/identifiers
-
-print("my name is :", name)
-print("my age is :", age)
-print("my hobbies are :", hobbies)
