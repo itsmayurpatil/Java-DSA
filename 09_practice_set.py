b83de13c12003a13780114897f2d13b5bfa3eebd
Mayur R. Patil <malpha1676@gmail.com>
2026-08-30 16:04:24 +0530

    Delete 09_practice_set.py

--- 09_practice_set.py
+++
@@ -1,7 +0,0 @@
-# 02 WAP to input 2 floating point no. & print  their average 
-
-a = float(input("enter first no :"))
-b = float(input("enter second no :"))
-
-print(" sum =", a + b)
-print(" avg =", (a + b)/2)
